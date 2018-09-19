76b91f731c7840106d13fc3c6b0a3170bec0d31f
KosNasyr <33783061+KosNasyr@users.noreply.github.com>
2018-09-19 21:10:18 +0300

    First Commit

---
+++ Sort10Numbers.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Sort10Numbers {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Введите число под номером =" + (i + 1));
+            nums.add(sc.nextInt());
+        }
+        /*
+        Collections.sort(nums);
+        Если вариат сортировки через Collections Вас не устраивает то:
+         */
+        for (int i = nums.size() - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums.get(j) > nums.get(j + 1)) {
+                    int tmp = nums.get(j);
+                    nums.set(j, nums.get(j + 1));
+                    nums.set(j + 1, tmp);
+                }
+            }
+        }
+        System.out.println("Числа в порядке возрастания = " + nums);
+        sc.close();
+    }
+}
